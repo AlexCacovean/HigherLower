33e5d36423d48f1f8fb7bae3f70c57ce0ff4ef33
Alex Cacovean <alex.r.cacovean@gmail.com>
2019-07-19 10:05:43 +0300

    Added Leaderboard UI

---
+++ HigherLower/HigherLower/Leaderboard.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace HigherLower
+{
+    partial class Leaderboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EighthPlacePointsLabel = new System.Windows.Forms.Label();
+            this.SevenPlacePointsLabel = new System.Windows.Forms.Label();
+            this.SixthPlacePointsLabel = new System.Windows.Forms.Label();
+            this.FifthPlacePointsLabel = new System.Windows.Forms.Label();
+            this.FourthPlacePointsLabel = new System.Windows.Forms.Label();
+            this.ThirdPlacePointsLabel = new System.Windows.Forms.Label();
+            this.SecondPlacePointsLabel = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.EighthPlaceNameLabel = new System.Windows.Forms.Label();
+            this.SevenPlaceNameLabel = new System.Windows.Forms.Label();
+            this.SixthPlaceNameLabel = new System.Windows.Forms.Label();
+            this.FifthPlaceNameLabel = new System.Windows.Forms.Label();
+            this.FourthPlaceNameLabel = new System.Windows.Forms.Label();
+            this.ThirdPlaceNameLabel = new System.Windows.Forms.Label();
+            this.SecondPlaceNameLabel = new System.Windows.Forms.Label();
+            this.FirstPlaceNameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EighthPlacePointsLabel
+            // 
+            this.EighthPlacePointsLabel.AutoSize = true;
+            this.EighthPlacePointsLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EighthPlacePointsLabel.Location = new System.Drawing.Point(162, 394);
+            this.EighthPlacePointsLabel.Name = "EighthPlacePointsLabel";
+            this.EighthPlacePointsLabel.Size = new System.Drawing.Size(136, 15);
+            this.EighthPlacePointsLabel.TabIndex = 48;
+            this.EighthPlacePointsLabel.Text = "EighthPlacePointsLabel";
+            // 
+            // SevenPlacePointsLabel
+            // 
+            this.SevenPlacePointsLabel.AutoSize = true;
+            this.SevenPlacePointsLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SevenPlacePointsLabel.Location = new System.Drawing.Point(168, 357);
+            this.SevenPlacePointsLabel.Name = "SevenPlacePointsLabel";
+            this.SevenPlacePointsLabel.Size = new System.Drawing.Size(129, 15);
+            this.SevenPlacePointsLabel.TabIndex = 47;
+            this.SevenPlacePointsLabel.Text = "SevenPlacePointsLabel";
+            // 
+            // SixthPlacePointsLabel
+            // 
+            this.SixthPlacePointsLabel.AutoSize = true;
+            this.SixthPlacePointsLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SixthPlacePointsLabel.Location = new System.Drawing.Point(151, 321);
+            this.SixthPlacePointsLabel.Name = "SixthPlacePointsLabel";
+            this.SixthPlacePointsLabel.Size = new System.Drawing.Size(127, 15);
+            this.SixthPlacePointsLabel.TabIndex = 46;
+            this.SixthPlacePointsLabel.Text = "SixthPlacePointsLabel";
+            // 
+            // FifthPlacePointsLabel
+            // 
+            this.FifthPlacePointsLabel.AutoSize = true;
+            this.FifthPlacePointsLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FifthPlacePointsLabel.Location = new System.Drawing.Point(159, 285);
+            this.FifthPlacePointsLabel.Name = "FifthPlacePointsLabel";
+            this.FifthPlacePointsLabel.Size = new System.Drawing.Size(127, 15);
+            this.FifthPlacePointsLabel.TabIndex = 45;
+            this.FifthPlacePointsLabel.Text = "FifthPlacePointsLabel";
+            // 
+            // FourthPlacePointsLabel
+            // 
+            this.FourthPlacePointsLabel.AutoSize = true;
+            this.FourthPlacePointsLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FourthPlacePointsLabel.Location = new System.Drawing.Point(159, 252);
+            this.FourthPlacePointsLabel.Name = "FourthPlacePointsLabel";
+            this.FourthPlacePointsLabel.Size = new System.Drawing.Size(136, 15);
+            this.FourthPlacePointsLabel.TabIndex = 44;
+            this.FourthPlacePointsLabel.Text = "FourthPlacePointsLabel";
+            // 
+            // ThirdPlacePointsLabel
+            // 
+            this.ThirdPlacePointsLabel.AutoSize = true;
+            this.ThirdPlacePointsLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThirdPlacePointsLabel.Location = new System.Drawing.Point(153, 215);
+            this.ThirdPlacePointsLabel.Name = "ThirdPlacePointsLabel";
+            this.ThirdPlacePointsLabel.Size = new System.Drawing.Size(129, 15);
+            this.ThirdPlacePointsLabel.TabIndex = 43;
+            this.ThirdPlacePointsLabel.Text = "ThirdPlacePointsLabel";
+            // 
+            // SecondPlacePointsLabel
+            // 
+            this.SecondPlacePointsLabel.AutoSize = true;
+            this.SecondPlacePointsLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SecondPlacePointsLabel.Location = new System.Drawing.Point(159, 176);
+            this.SecondPlacePointsLabel.Name = "SecondPlacePointsLabel";
+            this.SecondPlacePointsLabel.Size = new System.Drawing.Size(135, 15);
+            this.SecondPlacePointsLabel.TabIndex = 42;
+            this.SecondPlacePointsLabel.Text = "SecondPlacePointsLabel";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(403, 117);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(153, 18);
+            this.label11.TabIndex = 41;
+            this.label11.Text = "FirstPlacePointsLabel";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(684, 30);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(94, 13);
+            this.label10.TabIndex = 40;
+            this.label10.Text = "HIGHER/LOWER";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(652, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 38);
+            this.button1.TabIndex = 39;
+            this.button1.Text = "PLAY AGAIN";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // EighthPlaceNameLabel
+            // 
+            this.EighthPlaceNameLabel.AutoSize = true;
+            this.EighthPlaceNameLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EighthPlaceNameLabel.Location = new System.Drawing.Point(22, 394);
+            this.EighthPlaceNameLabel.Name = "EighthPlaceNameLabel";
+            this.EighthPlaceNameLabel.Size = new System.Drawing.Size(132, 15);
+            this.EighthPlaceNameLabel.TabIndex = 38;
+            this.EighthPlaceNameLabel.Text = "EighthPlaceNameLabel";
+            // 
+            // SevenPlaceNameLabel
+            // 
+            this.SevenPlaceNameLabel.AutoSize = true;
+            this.SevenPlaceNameLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SevenPlaceNameLabel.Location = new System.Drawing.Point(22, 356);
+            this.SevenPlaceNameLabel.Name = "SevenPlaceNameLabel";
+            this.SevenPlaceNameLabel.Size = new System.Drawing.Size(136, 15);
+            this.SevenPlaceNameLabel.TabIndex = 37;
+            this.SevenPlaceNameLabel.Text = "SeventhPlaceNameLabel";
+            // 
+            // SixthPlaceNameLabel
+            // 
+            this.SixthPlaceNameLabel.AutoSize = true;
+            this.SixthPlaceNameLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SixthPlaceNameLabel.Location = new System.Drawing.Point(22, 319);
+            this.SixthPlaceNameLabel.Name = "SixthPlaceNameLabel";
+            this.SixthPlaceNameLabel.Size = new System.Drawing.Size(123, 15);
+            this.SixthPlaceNameLabel.TabIndex = 36;
+            this.SixthPlaceNameLabel.Text = "SixthPlaceNameLabel";
+            // 
+            // FifthPlaceNameLabel
+            // 
+            this.FifthPlaceNameLabel.AutoSize = true;
+            this.FifthPlaceNameLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FifthPlaceNameLabel.Location = new System.Drawing.Point(22, 284);
+            this.FifthPlaceNameLabel.Name = "FifthPlaceNameLabel";
+            this.FifthPlaceNameLabel.Size = new System.Drawing.Size(123, 15);
+            this.FifthPlaceNameLabel.TabIndex = 35;
+            this.FifthPlaceNameLabel.Text = "FifthPlaceNameLabel";
+            // 
+            // FourthPlaceNameLabel
+            // 
+            this.FourthPlaceNameLabel.AutoSize = true;
+            this.FourthPlaceNameLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FourthPlaceNameLabel.Location = new System.Drawing.Point(22, 250);
+            this.FourthPlaceNameLabel.Name = "FourthPlaceNameLabel";
+            this.FourthPlaceNameLabel.Size = new System.Drawing.Size(132, 15);
+            this.FourthPlaceNameLabel.TabIndex = 34;
+            this.FourthPlaceNameLabel.Text = "FourthPlaceNameLabel";
+            // 
+            // ThirdPlaceNameLabel
+            // 
+            this.ThirdPlaceNameLabel.AutoSize = true;
+            this.ThirdPlaceNameLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThirdPlaceNameLabel.Location = new System.Drawing.Point(22, 213);
+            this.ThirdPlaceNameLabel.Name = "ThirdPlaceNameLabel";
+            this.ThirdPlaceNameLabel.Size = new System.Drawing.Size(125, 15);
+            this.ThirdPlaceNameLabel.TabIndex = 33;
+            this.ThirdPlaceNameLabel.Text = "ThirdPlaceNameLabel";
+            // 
+            // SecondPlaceNameLabel
+            // 
+            this.SecondPlaceNameLabel.AutoSize = true;
+            this.SecondPlaceNameLabel.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SecondPlaceNameLabel.Location = new System.Drawing.Point(22, 176);
+            this.SecondPlaceNameLabel.Name = "SecondPlaceNameLabel";
+            this.SecondPlaceNameLabel.Size = new System.Drawing.Size(131, 15);
+            this.SecondPlaceNameLabel.TabIndex = 32;
+            this.SecondPlaceNameLabel.Text = "SecondPlaceNameLabel";
+            // 
+            // FirstPlaceNameLabel
+            // 
+            this.FirstPlaceNameLabel.AutoSize = true;
+            this.FirstPlaceNameLabel.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstPlaceNameLabel.Location = new System.Drawing.Point(250, 117);
+            this.FirstPlaceNameLabel.Name = "FirstPlaceNameLabel";
+            this.FirstPlaceNameLabel.Size = new System.Drawing.Size(147, 18);
+            this.FirstPlaceNameLabel.TabIndex = 31;
+            this.FirstPlaceNameLabel.Text = "FirstPlaceNameLabel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(316, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 18);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "LEADERBOARD";
+            // 
+            // Leaderboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.EighthPlacePointsLabel);
+            this.Controls.Add(this.SevenPlacePointsLabel);
+            this.Controls.Add(this.SixthPlacePointsLabel);
+            this.Controls.Add(this.FifthPlacePointsLabel);
+            this.Controls.Add(this.FourthPlacePointsLabel);
+            this.Controls.Add(this.ThirdPlacePointsLabel);
+            this.Controls.Add(this.SecondPlacePointsLabel);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EighthPlaceNameLabel);
+            this.Controls.Add(this.SevenPlaceNameLabel);
+            this.Controls.Add(this.SixthPlaceNameLabel);
+            this.Controls.Add(this.FifthPlaceNameLabel);
+            this.Controls.Add(this.FourthPlaceNameLabel);
+            this.Controls.Add(this.ThirdPlaceNameLabel);
+            this.Controls.Add(this.SecondPlaceNameLabel);
+            this.Controls.Add(this.FirstPlaceNameLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Leaderboard";
+            this.Text = "Leaderboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EighthPlacePointsLabel;
+        private System.Windows.Forms.Label SevenPlacePointsLabel;
+        private System.Windows.Forms.Label SixthPlacePointsLabel;
+        private System.Windows.Forms.Label FifthPlacePointsLabel;
+        private System.Windows.Forms.Label FourthPlacePointsLabel;
+        private System.Windows.Forms.Label ThirdPlacePointsLabel;
+        private System.Windows.Forms.Label SecondPlacePointsLabel;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label EighthPlaceNameLabel;
+        private System.Windows.Forms.Label SevenPlaceNameLabel;
+        private System.Windows.Forms.Label SixthPlaceNameLabel;
+        private System.Windows.Forms.Label FifthPlaceNameLabel;
+        private System.Windows.Forms.Label FourthPlaceNameLabel;
+        private System.Windows.Forms.Label ThirdPlaceNameLabel;
+        private System.Windows.Forms.Label SecondPlaceNameLabel;
+        private System.Windows.Forms.Label FirstPlaceNameLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
